33dc058a9ba4009adc29dbeff1c8a1dbef6368d0
Vitor Pires <tor_pires@hotmail.com>
2024-05-04 23:04:49 -0300

    Implementing CRUD of tasks & Update

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ from models.task import Task
 app = Flask(__name__)
 
 #CRUD - Create, Read, Update, Delete 
-#Tabela: Tarefa 
-
 tasks = []
 task_id_control = 1
 
+#CREATE
 @app.route('/tasks', methods=['POST'])
 def create_task():
     global task_id_control
@@ -18,6 +17,60 @@ def create_task():
     tasks.append(new_task)
     print(tasks)
     return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+#READ
+@app.route('/tasks', methods = ['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+
+    output = {
+                "tasks": task_list,
+                "total_tasks": len(task_list)
+            }
+    
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods = ['GET'])
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+        
+    return jsonify({"message": "Não foi possivel encontrar atividade"}), 404
+
+#Update
+@app.route('/tasks/<int:id>', methods = ['PUT'])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+
+    if task == None:
+            return jsonify({"message": "Não foi possivel encontrar atividade"}), 404
+
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    return jsonify({'message': 'Tarefa atualizada com sucesso'})
+
+#Delete
+@app.route('/tasks/<int:id>', methods = ['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+
+    if task == None:
+            return jsonify({"message": "Não foi possivel encontrar atividade"}), 404
+
+    tasks.remove(task)
+    return jsonify({'message': 'Tarefa deletada com sucesso'})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
