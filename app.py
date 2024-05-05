575f6293e39c550cffc104abc767f2cd2e29d9c2
Vitor Pires <tor_pires@hotmail.com>
2024-05-05 20:44:43 -0300

    Adding  tests.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def create_task():
     task_id_control +=1
     tasks.append(new_task)
     print(tasks)
-    return jsonify({"message": "Nova tarefa criada com sucesso"})
+    return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})
 
 #READ
 @app.route('/tasks', methods = ['GET'])
@@ -70,7 +70,6 @@ def delete_task(id):
 
     tasks.remove(task)
     return jsonify({'message': 'Tarefa deletada com sucesso'})
-
 
 if __name__ == "__main__":
     app.run(debug=True)
